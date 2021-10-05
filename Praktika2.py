2733af8937ef703fe6fcf0385f2d2f889a02ff98
Viktor200719821974 <viktorvanyatanya@gmail.com>
2021-10-06 00:23:15 +0300

    Praktika 2 break

---
+++ Praktika2.py
@@ -0,0 +1,44 @@
+# 1.создать функцию которая будет возвращать сумму разрядов числа в виде строки
+#
+# Пример:
+#
+# expanded_form(12) # return '10 + 2'
+# expanded_form(42) # return '40 + 2'
+# expanded_form(70304) # return '70000 + 300 + 4'
+# def expanded_form(num):
+#     result = []
+#
+#     for index, digit in enumerate(str(num)[::-1]):
+#         print(digit)
+#         if int(digit) != 0:
+#             result.append(digit + ('0' * index))
+#
+#     return ' + '.join(result[::-1])
+#
+#
+# print(expanded_form(70304))
+
+
+# 2.создать функцию которая будет выводить сколько и каких символов в строке
+# пример:
+#
+# st = 'as 23 fdfdg544
+#
+# 'a' -> 1
+# 's' -> 1
+# ' ' -> 2
+# '2' -> 1
+# '3' -> 1
+# 'f' -> 2
+# 'd' -> 2
+# 'g' -> 1
+# '5' -> 1
+# '4' -> 2
+def check_freq(str):
+    freq = {}
+    for c in str:
+        freq[c] = str.count(c)
+    return freq
+
+
+print(check_freq("as 23 fdfdg544"))
