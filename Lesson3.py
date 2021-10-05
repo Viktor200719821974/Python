9ea903f6a4f57a80b79ab5d86137a036182043b7
Viktor200719821974 <viktorvanyatanya@gmail.com>
2021-10-05 23:28:37 +0300

    Lesson 3 break

--- Lesson3.py
+++ Lesson3.py
@@ -9,11 +9,38 @@ x3 = '09:00 Доїхати до праці'
 x4 = '17:00 Закінчити працю та виїхати додому'
 x5 = '18:00 Повечеряти, прийняти душ, зайнятися своїми справами, лягти спати'
 
+# def notebook():
+#     todo_list = []
+#
+#     def add_todo(*args):
+#         global x1, x2, x3, x4, x5
+#         todo_list.append(x1)
+#         todo_list.append(x2)
+#         todo_list.append(x3)
+#         todo_list.append(x4)
+#         todo_list.append(x5)
+#
+#         def get_all():
+#             for i in todo_list:
+#                 print(i)
+#
+#         get_all()
+#
+#     add_todo()
+#     return todo_list
+#
+#
+# notebook()
 
-def notebook():
+# 2) протипизировать первое задание
+import typing
+from typing import Callable, List, Tuple, Dict
+
+
+def notebook() -> List:
     todo_list = []
 
-    def add_todo(*args):
+    def add_todo(*args: str) -> None:
         global x1, x2, x3, x4, x5
         todo_list.append(x1)
         todo_list.append(x2)
@@ -27,13 +54,12 @@ def notebook():
 
         get_all()
 
-    add_todo(x1, x2, x3, x4, x5)
+    add_todo()
     return todo_list
 
 
 notebook()
 
-# 2) протипизировать первое задание
 # 3) С помощью lambda функции извлеките из списка числа, делимые на 15 без остатка.
 # my_list = [45, 56, 67, 776, 5677, 6887, 667, 788, 300]
 # f = filter(lambda x: x % 15 == 0, my_list)
