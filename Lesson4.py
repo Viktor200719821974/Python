9632b69b727f5e3943e88bc189eca71ff64f2ef1
Viktor200719821974 <viktorvanyatanya@gmail.com>
2021-10-08 18:55:14 +0300

    Lesson 4 read

--- Lesson4.py
+++ Lesson4.py
@@ -56,41 +56,92 @@
 # 5) при створені об'єкта, __count має збільшуватися на 1
 # 6) має бути метод(метод класу) для виводу __сount
 # 7) має бути метод який записує в наш ліст текст з нашої змінної __text
+l = []
 
+
+class Letter:
+    __count = 0
+
+    def __init__(self, text):
+        self.__text = text
+        self.__class__.__count += 1
+
+    @property
+    def text(self):
+        return self.__text
+
+    @text.setter
+    def text(self, new_value):
+        self.__text = new_value
+
+        # text = property(__gettext, __settext)
+
+    @classmethod
+    def get_count(cls):
+        return cls.__count
+
+    @classmethod
+    def set_count(cls):
+        print('Кількість:', cls.__count)
+
+    def add_text(self, text):
+        return print(l.append(self.__text))
+
+
+letter1 = Letter('должна быть переменная count которая будет')
+letter2 = Letter('должна быть переменная count которая будет')
+Letter.set_count()
+Letter.add_text(letter1, letter2)
 ###############################################################################
 # создать класс Human(name, age) создать два класса Prince и Cinderella:
 # у золушки должно быть имя возраст и размер ноги
 # у принца имя, возраст и размер найденой туфельки, так же должен быть метод который принимает лист золушек и ищет  ту самую
 # класса золушки должна быть переменная count которая будет считать сколько экземпляров класса золушка было создано
 # и метод класса который будет показывать это количество
-class Human:
-    def __init__(self, name, age):
-        self.name = name
-        self.age = age
-
-        # def find_cinderella()
-
-
-class Prince(Human):
-    def __init__(self, name, age, size_find):
-        super().__init__(name, age)
-        self.size_find = size_find
-
-
-class Cinderella(Human):
-    def __init__(self, name, age, size):
-        super().__init__(name, age)
-        self.size = size
-
-
-prince = Prince(name='Ivan', age=20, size_find=34)
-cinderella1 = Cinderella(name='Ola', age=15, size=36)
-cinderella2 = Cinderella(name='Tanya', age=23, size=35)
-cinderella3 = Cinderella(name='Ira', age=22, size=38)
-cinderella4 = Cinderella(name='Lena', age=24, size=39)
-cinderella5 = Cinderella(name='Rita', age=20, size=40)
-cinderella6 = Cinderella(name='Yana', age=18, size=37)
-cinderella7 = Cinderella(name='Masha', age=19, size=34)
-cinderella8 = Cinderella(name='Lesya', age=17, size=35)
-cinderella9 = Cinderella(name='Vera', age=25, size=42)
-cinderella10 = Cinderella(name='Tonya', age=17, size=41)
+# class Human:
+#     def __init__(self, name, age):
+#         self.name = name
+#         self.age = age
+#
+#
+# class Prince(Human):
+#     def __init__(self, name, age, size_find):
+#         super().__init__(name, age)
+#         self.size_find = size_find
+#
+#     def find_size(self):
+#         for i in cinderellas:
+#             if self.size_find == i.size:
+#                 print('Вітаю, ти знайшов свою попелюшку, її звуть:', i.name)
+#
+#
+# class Cinderella(Human):
+#     count = 0
+#
+#     def __init__(self, name, age, size):
+#         super().__init__(name, age)
+#         self.size = size
+#         self.__class__.count += 1
+#
+#     @classmethod
+#     def get_count(cls):
+#         return cls.count
+#
+#     @classmethod
+#     def set_count(cls):
+#         print('Кількість золушок:', cls.count)
+#
+#
+# prince = Prince(name='Ivan', age=20, size_find=34)
+# cinderellas = [Cinderella(name='Ola', age=15, size=36),
+#                Cinderella(name='Tanya', age=23, size=35),
+#                Cinderella(name='Ira', age=22, size=38),
+#                Cinderella(name='Lena', age=24, size=39),
+#                Cinderella(name='Rita', age=20, size=40),
+#                Cinderella(name='Yana', age=18, size=37),
+#                Cinderella(name='Masha', age=19, size=34),
+#                Cinderella(name='Lesya', age=17, size=35),
+#                Cinderella(name='Vera', age=25, size=42),
+#                Cinderella(name='Tonya', age=17, size=41)]
+# Cinderella.set_count()
+# prince.find_size()
