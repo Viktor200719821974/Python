65a1c1bc6bc001c7c838011a81e036043ed29839
Viktor200719821974 <viktorvanyatanya@gmail.com>
2021-10-14 15:43:21 +0300

    Praktika3 read

--- Praktika3.py
+++ Praktika3.py
@@ -1,18 +1,10 @@
 import json
 
-with open('users.txt', 'r') as file:
-    content = file.read()
-    fileLine = content.splitlines()
-    d = []
-    for i in fileLine:
-        l = i.split('|')
-        d.append(l)
-
-f = [x for xs in d for x in xs]
-
-d = []
-a = ['name', 'age', 'gender']
-b = a * int(len(f) / len(a))
-k = dict(zip(b, f))
-with open('my_file.json', 'w') as file:
-    json.dump(k, file)
+with open('users.txt') as file:
+    users = [user.strip() for user in file.readlines()]
+    res = []
+    for item in users:
+        name, age, gender = item.split('|')
+        res.append({'name': name, 'age': int(age), 'gender': gender})
+    with open('users.json', 'w') as file1:
+        json.dump(res, file1)
